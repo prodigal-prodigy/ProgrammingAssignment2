b5fabaf09f7a3a0ef7a7c9b90c09331a5bed6fee
prodigal-prodigy <arya.has.fun@gmail.com>
2017-11-07 21:26:20 +0530

    Second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,53 @@
 ##  a pair of functions that cache the inverse of a matrix
 
-## This function creates a special "matrix" object that can cache its inverse
+## 'makeCacheMatrix' -This function creates a special "matrix" object that can cache its inverse
+## 'makeCacheMatrix' returns a list of functions
+
+# set       set the value of the matrix
+# get       get the value of the matrix
+# setSolve  set the value of the inverted matrix
+# getsolve  get the value of the inverted matrix
+
+# passing x that is accessible to the functions inside makeCacheMatrix i.e.
+# 'set','get','setSolve' & 'getSolve'
 
 makeCacheMatrix <- function(x = matrix()) {
+# given below t is the cache and it is set to NULL  
   t <- NULL
+# 'set' function assigns the value 'x1' passed to it to the variable x
+# cache i.e. t is set to NULL
   set <- function(x1) {
     x <<- x1
     t <<- NULL
   }
+# 'get' function returns the variable x
   get <- function() x
-  setSolve <- function(solve) t <<- solve
+# 'setsolve' function caches the value of the inverted matrix
+  setSolve <- function(inverted_matrix) t <<- inverted_matrix
+# 'getsolve' function returns the cached value i.e. t  
   getSolve <- function() t
+# return the list of functions  
   list(set = set, get = get,
        setSolve = setSolve,
        getSolve = getSolve)
 }
 
 
-## This function computes the inverse of the special "matrix"
+## This function computes the inverse of the special "matrix" 
+## the matrix was created using 'makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Return a cached value stored in t
   t <- x$getSolve()
   if(!is.null(t)) {
     message("retrieving cached data")
     return(t)
   }
+# if the cache is empty then store the matrix  
   d <- x$get()
+# then get the inverse 
   t <- solve(d, ...)
-  x$setSolve(t)
-  m
+# store the inverse in cache
+    x$setSolve(t)
+  t
   }
